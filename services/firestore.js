cd4d3ae15098564194c2dd7a28bcb7697d1bd78c
Sava Stankovic <sava.stankovic2002@gmail.com>
2025-06-26 12:18:24 +0200

    feature(newsletter): firebase settings

---
+++ services/firestore.js
@@ -0,0 +1,45 @@
+const serviceAccount = require("../infinity-planners-firebase-adminsdk-fbsvc-b2def18657.json");
+const admin = require("firebase-admin"); // Import the Firebase Admin SDK
+require("dotenv").config();
+
+const firebaseCredentials = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
+
+admin.initializeApp({
+  credential: admin.credential.cert(firebaseCredentials),
+});
+
+const db = admin.firestore(); // This is how you get the Firestore instance with Admin SDK
+async function isEmailSubscribed(collectionName = "promo-codes", email) {
+  const docRef = await db.collection(collectionName).get();
+
+  const data = docRef.docs.map((doc) => doc.data());
+
+  const snapshot = data.find((present) => present.user_email === email);
+
+  if (typeof snapshot === "undefined") {
+    return false;
+  }
+  return true;
+}
+
+async function addToNewsletter(collectionName = "promo-codes", data) {
+  const docRef = await db
+    .collection(collectionName)
+    .doc(data.user_email)
+    .set(data);
+  return docRef;
+}
+
+module.exports = { isEmailSubscribed, addToNewsletter };
