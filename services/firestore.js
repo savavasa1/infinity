c4c02e211c7d6425a1026759f60349eaec7349a5
Sava Stankovic <sava.stankovic2002@gmail.com>
2025-06-26 13:05:57 +0200

    fix: no replacing in string

--- services/firestore.js
+++ services/firestore.js
@@ -5,7 +5,7 @@ const firebaseCredentials = {
   type: process.env.FIREBASE_TYPE,
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: process.env.FIREBASE_AUTH_URI,
@@ -16,10 +16,7 @@ const firebaseCredentials = {
 };
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    ...firebaseCredentials,
-    private_key: private_key.replace("\n", /\\n/g),
-  }),
+  credential: admin.credential.cert(firebaseCredentials),
 });
 
 const db = admin.firestore(); // This is how you get the Firestore instance with Admin SDK
