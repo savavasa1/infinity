c671ee88d6b686d0016541a0b18a26d53accec9a
Sava Stankovic <sava.stankovic2002@gmail.com>
2025-06-26 12:17:27 +0200

    feature(newsletter): scripts for generating promo code and checking if email is valid

---
+++ services/checkEmailValidity.js
@@ -0,0 +1,12 @@
+function checkEmailValidity(mail) {
+  const emailRegexp =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+  if (!emailRegexp.test(mail)) {
+    return false;
+  }
+
+  return true;
+}
+
+module.exports = checkEmailValidity;
