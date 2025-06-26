d37c12ff510416a6b5caf452573fc4e981d73d39
Sava Stankovic <sava.stankovic2002@gmail.com>
2025-06-26 13:51:31 +0200

    fix: all in one variable

--- services/firestore.js
+++ services/firestore.js
@@ -1,24 +1,11 @@
 const admin = require("firebase-admin"); // Import the Firebase Admin SDK
 require("dotenv").config();
 
-const firebaseCredentials = {
-  type: process.env.FIREBASE_TYPE,
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
-};
-
-console.log(firebaseCredentials, "firebase", firebaseCredentials.private_key);
+const serviceFromEnv = JSON.parse(process.env.FIREBASE_AUTH);
+console.log("firebase", serviceFromEnv);
 
 admin.initializeApp({
-  credential: admin.credential.cert(firebaseCredentials),
+  credential: admin.credential.cert(serviceFromEnv),
 });
 
 const db = admin.firestore(); // This is how you get the Firestore instance with Admin SDK
