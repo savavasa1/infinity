199959df28402bef7e81a8cf54224b8bb64163e4
Sava Stankovic <sava.stankovic2002@gmail.com>
2025-06-30 10:19:28 +0200

    feature(newsletter): applying promo code

---
+++ api/promo-code/apply.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { checkPromoCode } = require("../../services/firestore");
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use(express.json());
+
+app.post("/api/promo-code/apply", async (req, res) => {
+  const response = await checkPromoCode(req.body.code);
+
+  console.log(response);
+
+  res.json({ response: response, code: req.body.code });
+});
+
+// app.listen(7000, () => {
+//   console.log(`Server running on http://localhost:7000`);
+// });
+
+module.exports = app;
