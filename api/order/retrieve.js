448972b67aa914fa06a9278ca3949ba00c3233c0
Sava Stankovic <sava.stankovic2002@gmail.com>
2025-07-11 18:04:31 +0200

    fix(order): log in route

--- api/order/retrieve.js
+++ api/order/retrieve.js
@@ -14,6 +14,7 @@ app.use(express.json());
 
 app.post("/api/order/retrieve", async (req, res) => {
   const rqBdy = req.body;
+  console.log(rqBdy, "u retrive");
   const prod = await orderReceivedMail(rqBdy.payload.data, rqBdy.payload.id);
   //   const data = await response.json();
   //   console.log(data);
